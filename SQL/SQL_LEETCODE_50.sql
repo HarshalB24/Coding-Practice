d299b048e3e867383f2bf3cf08e425264216123b
Harshal <barangeharshal@gmail.com>
2026-09-18 22:42:10 +0530

    solved leetcode

--- SQL/SQL_LEETCODE_50.sql
+++ SQL/SQL_LEETCODE_50.sql
@@ -2513,3 +2513,88 @@ WHERE rn >= 7
 ORDER BY visited_on;
 
 here rn>= 7 means start showing records where first 7 row total exist
+
+----
+Table: RequestAccepted
+
++----------------+---------+
+| Column Name    | Type    |
++----------------+---------+
+| requester_id   | int     |
+| accepter_id    | int     |
+| accept_date    | date    |
++----------------+---------+
+(requester_id, accepter_id) is the primary key (combination of columns with unique values) for this table.
+This table contains the ID of the user who sent the request, the ID of the user who received the request, and the date when the request was accepted.
+ 
+
+Write a solution to find the people who have the most friends and the most friends number.
+
+The test cases are generated so that only one person has the most friends.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+RequestAccepted table:
++--------------+-------------+-------------+
+| requester_id | accepter_id | accept_date |
++--------------+-------------+-------------+
+| 1            | 2           | 2016/06/03  |
+| 1            | 3           | 2016/06/08  |
+| 2            | 3           | 2016/06/08  |
+| 3            | 4           | 2016/06/09  |
++--------------+-------------+-------------+
+Output: 
++----+-----+
+| id | num |
++----+-----+
+| 3  | 3   |
++----+-----+
+Explanation: 
+The person with id 3 is a friend of people 1, 2, and 4, so he has three friends in total, which is the most number than any others.
+ 
+
+Follow up: In the real world, multiple people could have the same most number of friends. Could you find all these people in this case?
+
+soln----
+
+1.so here to count id with most friends we can see requester id -> accepter id and accepter_id to requester_id but table counts only in 
+1 direction
+so we take the count of both ids and then union 
+
+with cte as (
+    select requester_id as id
+    from RequestAccepted
+    union all
+    select accepter_id as id
+    from RequestAccepted
+
+)
+select id , count(*) as num
+from cte
+group by id
+order by num desc
+limit 1
+
+----
+alternative : 
+WITH all_ids AS (
+   SELECT requester_id AS id 
+   FROM RequestAccepted
+   UNION ALL
+   SELECT accepter_id AS id
+   FROM RequestAccepted)
+SELECT id, num
+FROM 
+   (
+   SELECT id, 
+      COUNT(id) AS num, 
+      RANK () OVER(ORDER BY COUNT(id) DESC) AS rnk
+   FROM all_ids
+   GROUP BY id
+   )t0
+WHERE rnk=1
