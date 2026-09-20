7cc578148ce8bba00f6b4d86ca196a353f40bdc0
Harshal <barangeharshal@gmail.com>
2026-09-20 20:43:05 +0530

    solved datalemur

--- SQL/SQL_LEETCODE_50.sql
+++ SQL/SQL_LEETCODE_50.sql
@@ -3100,3 +3100,35 @@ select * , dense_rank () over (partition by user_id order by transaction_Date) a
 from transactions)
 elect user_id,spend,transaction_date from cte 
 where rn=3
+
+
+--------------------
+Imagine you're an HR analyst at a tech company tasked with analyzing employee salaries. Your manager is keen on understanding the pay distribution and asks you to determine the second highest salary among all employees.
+
+It's possible that multiple employees may share the same second highest salary. In case of duplicate, display the salary only once.
+
+employee Schema:
+column_name	type	description
+employee_id	integer	The unique ID of the employee.
+name	string	The name of the employee.
+salary	integer	The salary of the employee.
+department_id	integer	The department ID of the employee.
+manager_id	integer	The manager ID of the employee.
+employee Example Input:
+employee_id	name	salary	department_id	manager_id
+1	Emma Thompson	3800	1	6
+2	Daniel Rodriguez	2230	1	7
+3	Olivia Smith	2000	1	8
+Example Output:
+second_highest_salary
+2230
+The output represents the second highest salary among all employees. In this case, the second highest salary is $2,230.
+
+
+Soln -
+with cte as (
+select salary as second_highest_salary , row_number() over (order by salary desc) as rn
+from employee
+)
+select second_highest_salary from cte
+where rn=2
