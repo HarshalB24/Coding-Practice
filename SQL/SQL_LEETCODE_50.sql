ef2b0d7dff6827a9a02d7c7da83be4596c26ed12
Harshal <barangeharshal@gmail.com>
2026-09-20 20:42:09 +0530

    solved leetcode

--- SQL/SQL_LEETCODE_50.sql
+++ SQL/SQL_LEETCODE_50.sql
@@ -3070,3 +3070,33 @@ The mail of user 7 starts with a period.
 
 select * from users
 where regexp_like(mail,^[A-Za-z]+[A-za-A0-9._-]*@leetcode\\.com$,'c')
+
+
+
+#################################################################################
+DATA LEMUR QUESTIONS :
+
+1.Assume you are given the table below on Uber transactions made by users. Write a query to obtain the third transaction of every user. Output the user id, spend and transaction date.
+
+transactions Table:
+Column Name	Type
+user_id	integer
+spend	decimal
+transaction_date	timestamp
+transactions Example Input:
+user_id	spend	transaction_date
+111	100.50	01/08/2022 12:00:00
+111	55.00	01/10/2022 12:00:00
+121	36.00	01/18/2022 12:00:00
+145	24.99	01/26/2022 12:00:00
+111	89.60	02/05/2022 12:00:00
+Example Output:
+user_id	spend	transaction_date
+111	89.60	02/05/2022 12:00:00
+
+
+Soln - with cte as (
+select * , dense_rank () over (partition by user_id order by transaction_Date) as rn
+from transactions)
+elect user_id,spend,transaction_date from cte 
+where rn=3
