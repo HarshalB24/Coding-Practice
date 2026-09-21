de5ac78f4f05b5bcf2efa9de7b49e7c427ed233e
Harshal <barangeharshal@gmail.com>
2026-09-21 21:37:43 +0530

    solved datalemur

--- SQL/SQL_LEETCODE_50.sql
+++ SQL/SQL_LEETCODE_50.sql
@@ -3429,3 +3429,158 @@ select round(count(t.email_id):: decimal/ count(distinct e.email_id),2) as activ
 from emails e 
 left join texts t 
 on e.email_id=t.email_id and t.signup_Action='Confirmed'
+
+
+
+###################################################################################
+09/21/2026 
+Assume you're given a table Twitter tweet data, write a query to obtain a histogram of tweets posted per user in 2022. Output the tweet count per user as the bucket and the number of Twitter users who fall into that bucket.
+
+In other words, group the users by the number of tweets they posted in 2022 and count the number of users in each group.
+
+tweets Table:
+Column Name	Type
+tweet_id	integer
+user_id	integer
+msg	string
+tweet_date	timestamp
+tweets Example Input:
+tweet_id	user_id	msg	tweet_date
+214252	111	Am considering taking Tesla private at $420. Funding secured.	12/30/2021 00:00:00
+739252	111	Despite the constant negative press covfefe	01/01/2022 00:00:00
+846402	111	Following @NickSinghTech on Twitter changed my life!	02/14/2022 00:00:00
+241425	254	If the salary is so competitive why won’t you tell me what it is?	03/01/2022 00:00:00
+231574	148	I no longer have a manager. I can't be managed	03/23/2022 00:00:00
+Example Output:
+tweet_bucket	users_num
+1	2
+2	1
+Explanation:
+Based on the example output, there are two users who posted only one tweet in 2022, and one user who posted two tweets in 2022. The query groups the users by the number of tweets they posted and displays the number of users in each group.
+
+The dataset you are querying against may have different input & output - this is just an example!
+
+soln - 
+the mistake i did was i just couldnt simplify my solution
+--first find total tweets per user in 2022
+
+with cte as (
+select user_id , count(tweet_id) as tweets_per_user
+from tweets
+where extract (year from tweet_date)=2022
+group by user_id)
+select tweet_per_user as tweet_bucket , count(user_id) as users_num
+from cte
+group by tweet_per_user
+
+
+-------------
+Given a table of candidates and their skills, you're tasked with finding the candidates best suited for an open Data Science job. You want to find candidates who are proficient in Python, Tableau, and PostgreSQL.
+
+Write a query to list the candidates who possess all of the required skills for the job. Sort the output by candidate ID in ascending order.
+
+Assumption:
+
+There are no duplicates in the candidates table.
+candidates Table:
+Column Name	Type
+candidate_id	integer
+skill	varchar
+candidates Example Input:
+candidate_id	skill
+123	Python
+123	Tableau
+123	PostgreSQL
+234	R
+234	PowerBI
+234	SQL Server
+345	Python
+345	Tableau
+Example Output:
+candidate_id
+123
+Explanation
+Candidate 123 is displayed because they have Python, Tableau, and PostgreSQL skills. 345 isn't included in the output because they're missing one of the required skills: PostgreSQL.
+
+The dataset you are querying against may have different input & output - this is just an example!
+
+p.s. give the hints below a try if you're stuck and don't know where to start!
+
+
+soln- 
+select candidate_id from candidates
+where skill in ('python','tableau' , 'postgreSQL')
+
+
+
+-------------
+Assume you're given two tables containing data about Facebook Pages and their respective likes (as in "Like a Facebook Page").
+
+Write a query to return the IDs of the Facebook pages that have zero likes. The output should be sorted in ascending order based on the page IDs.
+
+pages Table:
+Column Name	Type
+page_id	integer
+page_name	varchar
+pages Example Input:
+page_id	page_name
+20001	SQL Solutions
+20045	Brain Exercises
+20701	Tips for Data Analysts
+page_likes Table:
+Column Name	Type
+user_id	integer
+page_id	integer
+liked_date	datetime
+page_likes Example Input:
+user_id	page_id	liked_date
+111	20001	04/08/2022 00:00:00
+121	20045	03/12/2022 00:00:00
+156	20001	07/25/2022 00:00:00
+Example Output:
+page_id
+20701
+
+
+select p.page_id from pages p 
+left join page_likes l 
+on 
+p.page_id=l.page_id
+where l.page_id is null
+
+
+--------------
+Tesla is investigating production bottlenecks and they need your help to extract the relevant data. Write a query to determine which parts have begun the assembly process but are not yet finished.
+
+Assumptions:
+
+parts_assembly table contains all parts currently in production, each at varying stages of the assembly process.
+An unfinished part is one that lacks a finish_date.
+This question is straightforward, so let's approach it with simplicity in both thinking and solution.
+
+Effective April 11th 2023, the problem statement and assumptions were updated to enhance clarity.
+
+parts_assembly Table
+Column Name	Type
+part	string
+finish_date	datetime
+assembly_step	integer
+parts_assembly Example Input
+part	finish_date	assembly_step
+battery	01/22/2022 00:00:00	1
+battery	02/22/2022 00:00:00	2
+battery	03/22/2022 00:00:00	3
+bumper	01/22/2022 00:00:00	1
+bumper	02/22/2022 00:00:00	2
+bumper		3
+bumper		4
+Example Output
+part	assembly_step
+bumper	3
+bumper	4
+Explanation
+The bumpers in step 3 and 4 are the only item that remains unfinished as it lacks a recorded finish date.
+
+
+select part, assembly_step from parts_assembly
+where finish_Date is null
